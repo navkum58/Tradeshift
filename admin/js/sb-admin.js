938b4ef5b7e7a43e424acf58911555cfb36c94e7
navkum58 <webdesign8787@gmail.com>
2018-08-21 10:14:50 +0530

    updated search profile market-manager tabs and markerplace page
    updated search profile market-manager tabs and markerplace page

--- admin/js/sb-admin.js
+++ admin/js/sb-admin.js
@@ -235,5 +235,38 @@ $('.close-dialog').click(function(){
   $('.create-product ').addClass('hide');
 
 });
+/*edit-variant-block*/
+$('.edit-variant-block').click(function(){
+                $('.overlay-full').show();
+  $('.edit-variant ').removeClass('hide');
+});
+$('.close-sidebar').click(function(){
+  $('.edit-variant').addClass('hide');
+});
+/*edit-variant-block ends*/
+$('.toggle-switch a').click(function(){
+  $('.toggle-switch a').removeClass('active');
+  $(this).addClass('active');
+});
+$('#carouselExample').on('slide.bs.carousel', function (e) {
 
+  
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
 })(jQuery); // End of use strict
